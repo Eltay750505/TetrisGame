bd02f724267484aa8c00ffdbd3c90e900f9dda3f
Eltay750505 <55701366+Eltay750505@users.noreply.github.com>
2019-12-02 17:55:25 +0300

    cofe
    added code

---
+++ Code/tetris/src/main/java/com/quirko/logic/bricks/TBrick.java
@@ -0,0 +1,43 @@
+package com.quirko.logic.bricks;
+
+import com.quirko.logic.MatrixOperations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class TBrick implements Brick {
+
+    private final List<int[][]> brickMatrix = new ArrayList<>();
+
+    public TBrick() {
+        brickMatrix.add(new int[][]{
+                {0, 0, 0, 0},
+                {1, 1, 1, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}
+        });
+        brickMatrix.add(new int[][]{
+                {0, 1, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}
+        });
+        brickMatrix.add(new int[][]{
+                {0, 1, 0, 0},
+                {1, 1, 1, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        });
+        brickMatrix.add(new int[][]{
+                {0, 1, 0, 0},
+                {1, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}
+        });
+    }
+
+    @Override
+    public List<int[][]> getShapeMatrix() {
+        return MatrixOperations.deepCopyList(brickMatrix);
+    }
+}
